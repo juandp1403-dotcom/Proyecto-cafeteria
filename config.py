bfae5f681a59f4297431a6af965f53c5f98c5b13
bduartesanabria-droid <bduartesanabria@gmail.com>
2026-06-24 14:53:37 -0500

    1.5

--- config.py
+++ config.py
@@ -3,14 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sena-cgao-cafeteria-2026-secret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(BASE_DIR, 'cafeteria.db')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Zona horaria Colombia
     TIMEZONE = 'America/Bogota'
 
 class DevelopmentConfig(Config):
@@ -21,6 +17,6 @@ class ProductionConfig(Config):
 
 config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'production':  ProductionConfig,
+    'default':     DevelopmentConfig
 }
