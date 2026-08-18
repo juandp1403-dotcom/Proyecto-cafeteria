f38549f3bdc41c160504e2fc7178eae1bac6e393
JuanDP1403 <juandp1403@gmail.com>
2026-08-18 16:45:10 -0500

    Juanes 5

--- models.py
+++ models.py
@@ -159,3 +159,25 @@ class DetalleCompra(db.Model):
             'nombre_producto': self.producto.nombre if self.producto else '',
             'cantidad':        self.cantidad,
         }
+
+class Reporte(db.Model):
+    __tablename__ = 'reporte'
+    idreporte  = db.Column(db.Integer, primary_key=True)
+    idadmin    = db.Column(db.Integer, db.ForeignKey('admin.documento'), nullable=False)
+    descripcion = db.Column(db.String(255), nullable=True)
+    fecha      = db.Column(db.Date, nullable=True, default=datetime.utcnow)
+    producto   = db.Column(db.Integer, db.ForeignKey('producto.idproducto'), nullable=False)
+
+    admin_rel  = db.relationship('Admin',    backref='reportes')
+    prod_rel   = db.relationship('Producto', backref='reportes')
+
+    def to_dict(self):
+        return {
+            'idreporte':   self.idreporte,
+            'idadmin':     self.idadmin,
+            'nombre_admin': self.admin_rel.nombre if self.admin_rel else '',
+            'descripcion': self.descripcion or '',
+            'fecha':       self.fecha.strftime('%d/%m/%Y') if self.fecha else '',
+            'idproducto':  self.producto,
+            'nombre_producto': self.prod_rel.nombre if self.prod_rel else '',
+        }
